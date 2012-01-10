bb36f665ae8de5c80fdea47fe5191d9dcceb8762
Dale Williams <dale@simpleweb.co.uk>
2012-01-10 12:12:29 +0000

    hidden comments form

--- js/script.js
+++ js/script.js
@@ -2,9 +2,12 @@ $(document).ready(function(){
 
 	// Fade in login prompt
 	$('#user-block .alert-message.tip').hide().delay(1000).fadeIn('slow');
-
 	// Fade out alert messages
 	$('.flash.alert-message').delay(2000).fadeOut('slow');
+	// Comments form
+	$('#comments-form').hide().slideIn('slow');
+
+
 
 	// Masonry stack magic
 	var $container = $('#recipes-list');
