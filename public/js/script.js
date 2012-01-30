c16daf9955703fed80c8f23946d0669cf455c292
Dale Williams <dale@simpleweb.co.uk>
2012-01-30 16:30:05 +0000

    layout changes

--- public/js/script.js
+++ public/js/script.js
@@ -5,19 +5,21 @@ $(document).ready(function(){
 	// Fade in login prompt
 	$('#user-block .alert-message.tip').hide().delay(1000).fadeIn('slow');
 
+	// cache container
+	var $container = $('#recipes-list');
+	// initialize isotope
+	$container.imagesLoaded( function(){
+	$container.isotope({
+		layoutMode: 'masonry',
+		itemSelector : '.teaser'
+	});
+	});
+
 	// filter items when filter link is clicked
-	$('#filters a').click(function(){
+	$('#order-by a').click(function(){
 	var selector = $(this).attr('data-filter');
 	$container.isotope({ filter: selector });
 	return false;
 	});
-
-
-
-
-	// Add alt margins to popular widget
-    $('#recipes-list .teaser').filter(function(index){
-    return (index%3 == 2);
-    }).addClass('last');
 
 });
